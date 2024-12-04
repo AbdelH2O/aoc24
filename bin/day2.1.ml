de7d489dddc526c55740d601a8cd509e00e55048
Hana <a.hana@aui.ma>
2024-12-04 12:44:09 +0100

    fix: properly do day2.1

--- bin/day2.1.ml
+++ bin/day2.1.ml
@@ -1,5 +1,3 @@
-let append_item lst a = lst @ [a]
-
 let rec diff elements last first =
   if first then
     match elements with
@@ -10,27 +8,44 @@ let rec diff elements last first =
     | [] -> []
     | x :: rest -> [(last - x)] @ diff rest x false
 
-let rec check polarity first elements = 
+let is_invalid num polarity =
+  abs(num) < 1 || abs(num) > 3 || ((num > 0) != polarity)
+
+let rec check polarity elements = 
   match elements with
   | [] -> 1
   | x :: rest -> (
-    if abs(x) < 1 || abs(x) > 3 || ((x > 0) != polarity) then
-      (if first then
-        check polarity false rest
-      else
-        0)
+    if is_invalid x polarity then
+      0
     else
-      check polarity first rest
+      check polarity rest
   )
+
+let rec get_polarity pos neg elements =
+  match elements with
+  | [] -> if pos > neg then true else false
+  | x :: rest -> if x > 0 then get_polarity (pos + 1) neg rest else (if x < 0 then get_polarity pos (neg + 1) rest else get_polarity pos neg rest)
+
+let rec check_all polarity prev rest =
+  match rest with
+  | [] -> check polarity (diff prev (List.nth prev 0) true)
+  | cur :: other -> if (check polarity (diff (prev @ other) (List.nth (prev @ other) 0) true)) == 1 then 1 else (check_all polarity (prev @ [cur]) other)
 
 let () = 
   let safe = ref 0 in
-  let inp = open_in "test.txt" in
+  let inp = open_in "input2.txt" in
 
   try
     while true do
-      let elements = input_line inp |> String.split_on_char ' ' |> List.filter (fun s -> s <> "") |> List.map (fun s -> int_of_string s) in
-      safe := !safe + (diff elements (List.nth elements 0) true |> (check (List.nth elements 0 > List.nth elements 1)) true);
+      let elements =
+        input_line inp
+        |> String.split_on_char ' '
+        |> List.filter (fun s -> s <> "")
+        |> List.map (fun s -> int_of_string s) in
+      let diffs = diff elements (List.nth elements 0) true in
+      let polarity = get_polarity 0 0 diffs in
+      
+      safe := !safe + (check_all polarity [] elements);
     done;
   with End_of_file ->
 
